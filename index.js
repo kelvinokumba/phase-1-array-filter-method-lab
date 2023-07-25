54956da61e361f4c29d3fd86ce9f8a038d8ad616
Kelvin Okumba <93493914+kelvinokumba@users.noreply.github.com>
2023-07-25 09:25:39 +0300

    completed

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
 // Code your solution here
-// Sample drivers array for testing purposes
-const drivers = [
-  { name: "John", hometown: "New York" },
-  { name: "Jane", hometown: "Chicago" },
-  { name: "Jake", hometown: "Los Angeles" },
-  { name: "James", hometown: "Miami" },
-];
 
 // Function to find matching drivers based on a provided string (case-insensitive)
 function findMatching(drivers, query) {
@@ -21,9 +14,4 @@ function fuzzyMatch(drivers, letters) {
 function matchName(drivers, name) {
   return drivers.filter((driver) => driver.name === name);
 }
-
-// Example usage and testing
-console.log(findMatching(["John", "Jane", "Jake", "James"], "john")); // Output: ["John"]
-console.log(fuzzyMatch(["John", "Jane", "Jake", "James"], "Ja")); // Output: ["Jane", "Jake", "James"]
-console.log(matchName(drivers, "John")); // Output: [{ name: "John", hometown: "New York" }]
 
